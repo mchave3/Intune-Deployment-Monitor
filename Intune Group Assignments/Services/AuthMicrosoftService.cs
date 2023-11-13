9bf2c9d020c1ca6df4072a1dde78093b15113bfb
Mickaël CHAVE <mchave3@live.fr>
2023-11-13 16:05:46 +0100

    updates

---
+++ Intune Group Assignments/Services/AuthMicrosoftService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intune_Group_Assignments.Services;
+internal class AuthMicrosoftService
+{
+}
