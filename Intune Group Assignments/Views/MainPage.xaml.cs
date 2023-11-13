22b89b5189a8ba4973bc5d6c23f76664a622c71f
Mickaël CHAVE <mchave3@live.fr>
2023-11-13 15:40:38 +0100

    updates

--- Intune Group Assignments/Views/MainPage.xaml.cs
+++ Intune Group Assignments/Views/MainPage.xaml.cs
@@ -1,5 +1,6 @@
-﻿using Intune_Group_Assignments.ViewModels;
-
+﻿using System.Diagnostics;
+using Intune_Group_Assignments.ViewModels;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace Intune_Group_Assignments.Views;
@@ -15,5 +16,41 @@ public sealed partial class MainPage : Page
     {
         ViewModel = App.GetService<MainViewModel>();
         InitializeComponent();
+
+        // Attach the event handler to the button click event
+        ButtonConnect.Click += ButtonConnect_Click;
+        ButtonDisconnect.Click += ButtonDisconnect_Click;
+    }
+
+    // Event handler for connection button
+    private void ButtonConnect_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            Debug.WriteLine("Toto");
+            // Create an instance of AuthMicrosoftService
+            // AuthMicrosoftService authMicrosoftService = new AuthMicrosoftService();
+
+            // Call the AuthMicrosoft method on the instance
+            // await AuthMicrosoftService.AuthMicrosoft();
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error: {ex.ToString()}");
+        }
+    }
+
+    // Event handler for disconnect button
+    private async void ButtonDisconnect_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            // Call the method on the existing instance
+            // await _authMicrosoftService.LogoutAsync();
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error: {ex.ToString()}");
+        }
     }
 }
