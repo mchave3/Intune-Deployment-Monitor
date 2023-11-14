b5882b5921a65c7c58cbcfc6b6e5069056c67649
Mickaël CHAVE <mchave3@live.fr>
2023-11-14 17:07:41 +0100

    updates

--- Intune Group Assignments/Services/MicrosoftGraphService.cs
+++ Intune Group Assignments/Services/MicrosoftGraphService.cs
@@ -1,49 +1,10 @@
-﻿using Microsoft.Graph;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using System.Diagnostics; // Required for debug logging
 
-namespace Intune_Group_Assignments.Services
+namespace Intune_Group_Assignments.Services;
+internal class MicrosoftGraphService
 {
-    internal class MicrosoftGraphService
-    {
-        private readonly GraphServiceClient _graphServiceClient;
-
-        // Constructor to initialize the GraphServiceClient
-        public MicrosoftGraphService(GraphServiceClient graphServiceClient)
-        {
-            _graphServiceClient = graphServiceClient;
-        }
-
-        // Method to get the User Principal Name (UPN) of the logged-in user
-        public async Task<string> GetUserUPNAsync()
-        {
-            try
-            {
-                // Fetching user details from Microsoft Graph
-                var user = await _graphServiceClient.Me.Request().GetAsync();
-
-                // Logging the retrieved UPN for debugging purposes
-                Debug.WriteLine($"User Principal Name retrieved: {user.UserPrincipalName}");
-
-                return user.UserPrincipalName;
-            }
-            catch (ServiceException ex)
-            {
-                // Logging the exception for debugging
-                Debug.WriteLine($"Error in fetching user UPN: {ex.Message}");
-
-                // Rethrowing the exception to be handled elsewhere
-                throw;
-            }
-            catch (Exception ex)
-            {
-                // General exception handling
-                Debug.WriteLine($"General error: {ex.Message}");
-
-                // Rethrowing the exception
-                throw;
-            }
-        }
-    }
 }
