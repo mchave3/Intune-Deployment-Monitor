df962be20e2410748d057a86671b744470dccb26
Mickaël CHAVE <mchave3@live.fr>
2023-11-15 15:29:45 +0100

    updates

--- Intune Group Assignments/ViewModels/AllDataViewModel.cs
+++ Intune Group Assignments/ViewModels/AllDataViewModel.cs
@@ -1,17 +1,35 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
-using System;
-using System.Windows.Input;
-using Intune_Group_Assignments.Services;
-using Microsoft.UI.Xaml;
-using System.Diagnostics;
-
-namespace Intune_Group_Assignments.ViewModels;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 public partial class AllDataViewModel : ObservableRecipient
 {
+    public ObservableCollection<SampleData> SampleTable
+    {
+        get; set;
+    }
+
     public AllDataViewModel()
     {
-    
+        // Initialize the ObservableCollection
+        SampleTable = new ObservableCollection<SampleData>();
+
+        // Add some sample data to the collection
+        SampleTable.Add(new SampleData { Name = "Boat 1", Value = "Value 1" });
+        SampleTable.Add(new SampleData { Name = "Boat 2", Value = "Value 2" });
+        SampleTable.Add(new SampleData { Name = "Boat 3", Value = "Value 3" });
+        // ... add more data as needed
+    }
+}
+
+// Define a simple data model for the sample data
+public class SampleData
+{
+    public string Name
+    {
+        get; set;
+    }
+    public string Value
+    {
+        get; set;
     }
 }
