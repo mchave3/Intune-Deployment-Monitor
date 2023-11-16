e3f109a2b3fb7e027630923c8a8cd73b165d7295
Mickaël CHAVE <mchave3@live.fr>
2023-11-16 16:46:36 +0100

    - Mise en place de la méthode GetAllDataAsync() - Traitement des données à faire #15

--- Intune Group Assignments/Services/MicrosoftGraphService.cs
+++ Intune Group Assignments/Services/MicrosoftGraphService.cs
@@ -12,7 +12,7 @@ public class MicrosoftGraphService
     // Base URL for Microsoft Graph API
     private readonly string baseGraphUrl = "https://graph.microsoft.com";
     // API version for Microsoft Graph
-    private readonly string apiVersion = "v1.0";
+    private readonly string apiVersion = "Beta";
 
     public async Task<string> GetUserDisplayNameAsync()
     {
@@ -58,5 +58,67 @@ public class MicrosoftGraphService
             Debug.WriteLine($"Exception occurred: {ex.Message}");
             return null;
         }
+    }
+
+    public async Task<List<(string ResourceName, string JsonResponse)>> GetAllDataAsync()
+    {
+        var resources = new List<(string Url, string Name)>
+        {
+            ("deviceManagement/configurationPolicies", "Configuration Policies"),
+            ("deviceManagement/deviceCompliancePolicies", "Device Compliance Policies"),
+            ("deviceManagement/deviceConfigurations", "Device Configurations"),
+            ("deviceManagement/deviceHealthScripts", "Device Health Scripts"),
+            ("deviceManagement/deviceManagementScripts", "Device Management Scripts"),
+            ("deviceManagement/groupPolicyConfigurations", "Group Policy Configurations"),
+            ("deviceAppManagement/mobileAppConfigurations", "Mobile App Configurations"),
+            ("deviceManagement/windowsAutopilotDeploymentProfiles", "Windows Autopilot Deployment Profiles"),
+            ("deviceAppManagement/mobileApps", "Applications"),
+            ("deviceAppManagement/androidManagedAppProtections", "Android Managed App Protections"),
+            ("deviceAppManagement/targetedManagedAppConfigurations", "App Configurations"),
+            ("deviceAppManagement/iosManagedAppProtections", "iOS Managed App Protections"),
+            ("deviceAppManagement/mdmWindowsInformationProtectionPolicies", "MDM Windows Information Protection Policies"),
+            ("deviceAppManagement/windowsManagedAppProtections", "Windows Managed App Protections")
+        };
+
+        var results = new List<(string ResourceName, string JsonResponse)>();
+
+        // Check if the token is near expiration and renew if necessary
+        if (AuthMicrosoftService.IsTokenNearExpiry())
+        {
+            await AuthMicrosoftService.RenewTokenSilently();
+        }
+
+        var accessToken = AuthMicrosoftService.GraphApiAccessToken;
+
+        using var client = new HttpClient();
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+        foreach (var (Url, Name) in resources)
+        {
+            var requestUrl = $"{baseGraphUrl}/{apiVersion}/{Url}?`$expand=assignments";
+            Debug.WriteLine($"Sending request to Microsoft Graph API: {requestUrl}");
+
+            try
+            {
+                var response = await client.GetAsync(requestUrl);
+                if (response.IsSuccessStatusCode)
+                {
+
+                    // Traitement des infos à faire dans cette partie
+
+                    var json = await response.Content.ReadAsStringAsync();
+                    results.Add((Name, json)); // Store both the name and the JSON response
+                }
+                else
+                {
+                    Debug.WriteLine($"Error in response for {Name}. Status Code: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Exception occurred while getting assignments for {Name}: {ex.Message}");
+            }
+        }
+        return results;
     }
 }
