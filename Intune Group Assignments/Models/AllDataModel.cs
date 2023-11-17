e667090f7ab734a1d8bb4a6bbedcc6e028459b2a
Mickaël CHAVE <mchave3@live.fr>
2023-11-17 16:20:22 +0100

    Réajustement du code AllDataModel.cs + ajout d'un padding bottom de 20pixel au grid de AllDataPage.xaml #22

--- Intune Group Assignments/Models/AllDataModel.cs
+++ Intune Group Assignments/Models/AllDataModel.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Diagnostics;
 using System.Net.Http.Headers;
-using System.Text;
 using System.Threading.Tasks;
-using Intune_Group_Assignments.Services;
 using Newtonsoft.Json;
+using Intune_Group_Assignments.Services;
+using System.Collections.Generic;
 
 namespace Intune_Group_Assignments.Models
 {
     class AllDataModel
     {
-        // Base URL for Microsoft Graph API
         private readonly string baseGraphUrl = "https://graph.microsoft.com";
-        // API version for Microsoft Graph
         private readonly string apiVersion = "Beta";
 
         public class ConfigurationPoliciesResponse
@@ -75,98 +70,105 @@ namespace Intune_Group_Assignments.Models
             }
         }
 
+        public async Task<List<(string PolicyName, string GroupId, string ResourceName)>> GetAllDataAsync()
+        {
+            await RenewTokenIfNeeded();
+
+            using var client = CreateHttpClient();
+            var allResults = new List<(string PolicyName, string GroupId, string ResourceName)>();
+
+            foreach (var (Url, Name) in GetResources())
+            {
+                await ProcessResource(client, Url, Name, allResults);
+            }
+
+            DebugAllResults(allResults);
+            return allResults;
+        }
+
+        private async Task RenewTokenIfNeeded()
+        {
+            if (AuthMicrosoftService.IsTokenNearExpiry())
+            {
+                await AuthMicrosoftService.RenewTokenSilently();
+            }
+        }
+
+        private HttpClient CreateHttpClient()
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", AuthMicrosoftService.GraphApiAccessToken);
+            return client;
+        }
+
+        private List<(string Url, string Name)> GetResources()
+        {
+            return new List<(string Url, string Name)>
+            {
+                ("deviceManagement/configurationPolicies", "Configuration Policies"),
+                ("deviceManagement/deviceCompliancePolicies", "Device Compliance Policies"),
+                ("deviceManagement/deviceConfigurations", "Device Configurations"),
+                ("deviceManagement/deviceHealthScripts", "Device Health Scripts"),
+                ("deviceManagement/deviceManagementScripts", "Device Management Scripts"),
+                ("deviceManagement/groupPolicyConfigurations", "Group Policy Configurations"),
+                ("deviceAppManagement/mobileAppConfigurations", "Mobile App Configurations"),
+                ("deviceManagement/windowsAutopilotDeploymentProfiles", "Windows Autopilot Deployment Profiles"),
+                ("deviceAppManagement/mobileApps", "Applications"),
+                ("deviceAppManagement/androidManagedAppProtections", "Android Managed App Protections"),
+                ("deviceAppManagement/targetedManagedAppConfigurations", "App Configurations"),
+                ("deviceAppManagement/iosManagedAppProtections", "iOS Managed App Protections"),
+                ("deviceAppManagement/mdmWindowsInformationProtectionPolicies", "MDM Windows Information Protection Policies"),
+                ("deviceAppManagement/windowsManagedAppProtections", "Windows Managed App Protections")
+            };
+        }
+
+        private async Task ProcessResource(HttpClient client, string url, string name, List<(string PolicyName, string GroupId, string ResourceName)> allResults)
+        {
+            var requestUrl = $"{baseGraphUrl}/{apiVersion}/{url}?expand=assignments";
+            try
+            {
+                var response = await client.GetAsync(requestUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    allResults.AddRange(await ProcessJsonResponse(name, json));
+                }
+                else
+                {
+                    Debug.WriteLine($"Error in response for {name}. Status Code: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Exception occurred while getting assignments for {name}: {ex.Message}");
+            }
+        }
+
         private async Task<List<(string PolicyName, string GroupId, string ResourceName)>> ProcessJsonResponse(string resourceName, string json)
         {
-            Debug.WriteLine($"JSON 2: {json}");
             var policiesResponse = JsonConvert.DeserializeObject<ConfigurationPoliciesResponse>(json);
             var result = new List<(string PolicyName, string GroupId, string ResourceName)>();
 
             foreach (var policy in policiesResponse.Value)
             {
-                // Check if Assignments is null
-                if (policy.Assignments != null)
+                if (policy.Assignments == null) continue;
+
+                foreach (var assignment in policy.Assignments)
                 {
-                    foreach (var assignment in policy.Assignments)
-                    {
-                        var groupId = assignment.Target.GroupId;
-                        Debug.WriteLine($"GroupId: {groupId}");
-                        var policyName = string.IsNullOrEmpty(policy.Name) ? "No DisplayName" : policy.Name;
-                        Debug.WriteLine($"PolicyName: {policyName}");
-                        result.Add((policyName, groupId, resourceName));
-                        Debug.WriteLine($"Result: {result}");
-                    }
-                }
-                else
-                {
-                    Debug.WriteLine($"Assignments is null for {policy.Name}");
+                    var groupId = assignment.Target.GroupId;
+                    var policyName = string.IsNullOrEmpty(policy.Name) ? "No DisplayName" : policy.Name;
+                    result.Add((policyName, groupId, resourceName));
                 }
             }
             return result;
         }
 
-        public async Task<List<(string PolicyName, string GroupId, string ResourceName)>> GetAllDataAsync()
+        private void DebugAllResults(List<(string PolicyName, string GroupId, string ResourceName)> allResults)
         {
-            var resources = new List<(string Url, string Name)>
-        {
-            ("deviceManagement/configurationPolicies", "Configuration Policies"),
-            ("deviceManagement/deviceCompliancePolicies", "Device Compliance Policies"),
-            ("deviceManagement/deviceConfigurations", "Device Configurations"),
-            ("deviceManagement/deviceHealthScripts", "Device Health Scripts"),
-            ("deviceManagement/deviceManagementScripts", "Device Management Scripts"),
-            ("deviceManagement/groupPolicyConfigurations", "Group Policy Configurations"),
-            ("deviceAppManagement/mobileAppConfigurations", "Mobile App Configurations"),
-            ("deviceManagement/windowsAutopilotDeploymentProfiles", "Windows Autopilot Deployment Profiles"),
-            ("deviceAppManagement/mobileApps", "Applications"),
-            ("deviceAppManagement/androidManagedAppProtections", "Android Managed App Protections"),
-            ("deviceAppManagement/targetedManagedAppConfigurations", "App Configurations"),
-            ("deviceAppManagement/iosManagedAppProtections", "iOS Managed App Protections"),
-            ("deviceAppManagement/mdmWindowsInformationProtectionPolicies", "MDM Windows Information Protection Policies"),
-            ("deviceAppManagement/windowsManagedAppProtections", "Windows Managed App Protections")
-        };
-
-            // Check if the token is near expiration and renew if necessary
-            if (AuthMicrosoftService.IsTokenNearExpiry())
-            {
-                await AuthMicrosoftService.RenewTokenSilently();
-            }
-
-            var accessToken = AuthMicrosoftService.GraphApiAccessToken;
-
-            using var client = new HttpClient();
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-
-            var allResults = new List<(string PolicyName, string GroupId, string ResourceName)>();
-
-            foreach (var (Url, Name) in resources)
-            {
-                var requestUrl = $"{baseGraphUrl}/{apiVersion}/{Url}?expand=assignments";
-                Debug.WriteLine($"Sending request to Microsoft Graph API: {requestUrl}");
-
-                try
-                {
-                    var response = await client.GetAsync(requestUrl);
-                    if (response.IsSuccessStatusCode)
-                    {
-                        Debug.WriteLine($"Response received for {Name}");
-                        var json = await response.Content.ReadAsStringAsync();
-                        Debug.WriteLine($"JSON 1: {json}");
-                        allResults.AddRange((List<(string PolicyName, string GroupId, string ResourceName)>?)await ProcessJsonResponse(Name, json));
-                    }
-                    else
-                    {
-                        Debug.WriteLine($"Error in response for {Name}. Status Code: {response.StatusCode}");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Debug.WriteLine($"Exception occurred while getting assignments for {Name}: {ex.Message}");
-                }
-            }
             foreach (var result in allResults)
             {
                 Debug.WriteLine($"Policy Name: {result.PolicyName}, Group ID: {result.GroupId}, Resource Name: {result.ResourceName}");
             }
-            return allResults;
         }
     }
 }
