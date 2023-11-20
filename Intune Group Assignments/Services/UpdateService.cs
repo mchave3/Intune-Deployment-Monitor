9804657f2563a3aae05adc4f8588a071fb2dbc94
Mickaël CHAVE <mchave3@live.fr>
2023-11-20 16:44:38 +0100

    Mise en place de la vérification des MAJ. Ajout de UpdateService et UpdateViewModel #25 #26 #27 #28

---
+++ Intune Group Assignments/Services/UpdateService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+public class UpdateService
+{
+    private const string LatestReleaseUrl = "https://api.github.com/repos/mchave3/Intune-Group-Assignments/releases/latest";
+
+    public async Task<string> CheckForUpdatesAsync()
+    {
+        using (var client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Add("User-Agent", "request");
+            var response = await client.GetStringAsync(LatestReleaseUrl);
+            dynamic latestRelease = JsonConvert.DeserializeObject(response);
+            return latestRelease.tag_name;
+        }
+    }
+
+    public async Task<string> DownloadUpdateAsync(string downloadUrl)
+    {
+        var httpClient = new HttpClient();
+        var response = await httpClient.GetAsync(downloadUrl, HttpCompletionOption.ResponseHeadersRead);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception("Error downloading the update.");
+        }
+
+        var contentStream = await response.Content.ReadAsStreamAsync();
+        var fileName = Path.Combine(Path.GetTempPath(), "update.msi");
+
+        using (var fileStream = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None, 8192, true))
+        {
+            await contentStream.CopyToAsync(fileStream);
+        }
+
+        return fileName;
+    }
+
+    public void InstallUpdate(string filePath)
+    {
+        var processStartInfo = new ProcessStartInfo
+        {
+            FileName = filePath,
+            Arguments = "/silent",
+            UseShellExecute = true
+        };
+
+        Process.Start(processStartInfo);
+    }
+
+    public string GetDownloadUrl(string version)
+    {
+        // Remove the 'v' prefix from the GitHub version if present
+        if (version.StartsWith("v"))
+        {
+            version = version.Substring(1);
+        }
+
+        // Construct the download URL based on the version number
+        return $"https://github.com/mchave3/Intune-Group-Assignments/releases/download/v{version}/update.msi";
+    }
+}
---
+++ Intune Group Assignments/ViewModels/UpdateViewModel.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml.Controls;
+using System.Xml;
+using System.IO;
+
+public class UpdateViewModel : INotifyPropertyChanged
+{
+    private readonly UpdateService _updateService;
+    private string _latestVersion;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public string LatestVersion
+    {
+        get => _latestVersion;
+        set
+        {
+            _latestVersion = value;
+            OnPropertyChanged(nameof(LatestVersion));
+        }
+    }
+
+    public ICommand CheckForUpdatesCommand
+    {
+        get;
+    }
+
+    public UpdateViewModel()
+    {
+        _updateService = new UpdateService();
+        CheckForUpdatesCommand = new RelayCommand(async () => await CheckForUpdates());
+    }
+
+    private async Task CheckForUpdates()
+    {
+        try
+        {
+            var latestVersion = await _updateService.CheckForUpdatesAsync();
+            var currentVersion = GetCurrentVersion();
+
+            if (IsNewVersionAvailable(currentVersion, latestVersion))
+            {
+                LatestVersion = latestVersion;
+                var dialog = new ContentDialog
+                {
+                    Title = "Update Available",
+                    Content = $"A new version {latestVersion} is available. Would you like to update now?",
+                    PrimaryButtonText = "Yes",
+                    SecondaryButtonText = "Cancel"
+                };
+
+                var result = await dialog.ShowAsync();
+
+                if (result == ContentDialogResult.Primary)
+                {
+                    await PerformUpdate(latestVersion);
+                }
+            }
+        }
+        catch (Exception)
+        {
+            var errorDialog = new ContentDialog
+            {
+                Title = "Error",
+                Content = "An error occurred while checking for updates.",
+                CloseButtonText = "Ok"
+            };
+
+            await errorDialog.ShowAsync();
+        }
+    }
+
+    private bool IsNewVersionAvailable(string currentVersion, string latestVersion)
+    {
+        // Normalize the versions to have the same number of segments
+        var currentVersionSegments = currentVersion.Split('.');
+        var latestVersionSegments = latestVersion.Split('.');
+
+        while (latestVersionSegments.Length < currentVersionSegments.Length)
+        {
+            latestVersion += ".0";
+            latestVersionSegments = latestVersion.Split('.');
+        }
+
+        return !string.Equals(currentVersion, latestVersion, StringComparison.Ordinal);
+    }
+
+    private string GetCurrentVersion()
+    {
+        var manifestPath = Path.Combine(Environment.CurrentDirectory, "Package.appxmanifest");
+        var xmlDoc = new XmlDocument();
+        xmlDoc.Load(manifestPath);
+        var identityNode = xmlDoc.DocumentElement.SelectSingleNode("/Package/Identity");
+        var version = identityNode.Attributes["Version"].Value;
+        return version;
+    }
+
+    private async Task PerformUpdate(string latestVersion)
+    {
+        var downloadUrl = _updateService.GetDownloadUrl(latestVersion);
+        var downloadedFilePath = await _updateService.DownloadUpdateAsync(downloadUrl);
+        _updateService.InstallUpdate(downloadedFilePath);
+    }
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
