c84c5e9a44b4f9d8ba05b6262c87a8d83dfbb9fa
Mickaël CHAVE <mchave3@live.fr>
2023-11-30 19:15:50 +0100

    Avancement du rework du page landing

--- Intune Deployment Monitor/ViewModels/HomeViewModel.cs
+++ Intune Deployment Monitor/ViewModels/HomeViewModel.cs
@@ -5,157 +5,186 @@ using System.Windows.Input;
 using Intune_Deployment_Monitor.Services;
 using Microsoft.UI.Xaml;
 using System.Diagnostics;
+using System.Reflection;
+using System.Threading.Tasks;
 
-namespace Intune_Deployment_Monitor.ViewModels;
-
-public partial class HomeViewModel : ObservableRecipient
+namespace Intune_Deployment_Monitor.ViewModels
 {
-    private readonly UpdateViewModel _updateViewModel;
-    private readonly MicrosoftGraphService _microsoftGraphService;
-
-    private Visibility _connectButtonVisibility = Visibility.Visible;
-    private Visibility _disconnectButtonVisibility = Visibility.Collapsed;
-    private string _displayName;
-
-    public HomeViewModel()
+    // ViewModel for the Home page that handles user login state and UI updates
+    public partial class HomeViewModel : ObservableRecipient
     {
-        _updateViewModel = new UpdateViewModel();
-        _microsoftGraphService = new MicrosoftGraphService();
+        // Services used by the ViewModel
+        private readonly UpdateViewModel _updateViewModel;
+        private readonly MicrosoftGraphService _microsoftGraphService;
 
-        ConnectCommand = new RelayCommand(ExecuteConnect);
-        DisconnectCommand = new RelayCommand(ExecuteDisconnect);
-        TestCommand = new RelayCommand(ExecuteTest);
-        SilentLogin();
-    }
+        // Fields to keep track of UI element visibility
+        private Visibility _connectButtonVisibility = Visibility.Visible;
+        private Visibility _disconnectButtonVisibility = Visibility.Collapsed;
+        private bool _isLoggedIn;
 
-    public ICommand ConnectCommand
-    {
-        get;
-    }
-    public ICommand DisconnectCommand
-    {
-        get;
-    }
+        // Fields to display user information
+        private string _displayName;
+        private string _versionNumber;
 
-    public ICommand TestCommand
-    {
-    
-           get;
-    }
-
-    public Visibility ConnectButtonVisibility
-    {
-        get => _connectButtonVisibility;
-        set => SetProperty(ref _connectButtonVisibility, value);
-    }
-
-    public Visibility DisconnectButtonVisibility
-    {
-        get => _disconnectButtonVisibility;
-        set => SetProperty(ref _disconnectButtonVisibility, value);
-    }
-
-    public async Task CheckForUpdates(UIElement uiElement)
-    {
-        await _updateViewModel.CheckForUpdates(uiElement);
-    }
-
-    public Visibility WelcomeMessageVisibility { get; private set; } = Visibility.Collapsed;
-
-    public string DisplayName
-    {
-        get => _displayName;
-        set
+        // Constructor initializing commands and services
+        public HomeViewModel()
         {
-            SetProperty(ref _displayName, value);
-            UpdateWelcomeMessageVisibility();
+            _updateViewModel = new UpdateViewModel();
+            _microsoftGraphService = new MicrosoftGraphService();
+
+            ConnectCommand = new RelayCommand(ExecuteConnect);
+            DisconnectCommand = new RelayCommand(ExecuteDisconnect);
+
+            VersionNumber = GetVersionNumber();
+            SilentLogin();
         }
-    }
 
-    private void UpdateWelcomeMessageVisibility()
-    {
-        WelcomeMessageVisibility = string.IsNullOrEmpty(DisplayName) ? Visibility.Collapsed : Visibility.Visible;
-        OnPropertyChanged(nameof(WelcomeMessageVisibility));
-    }
-
-    private async void ExecuteConnect()
-    {
-        await ExecuteConnectAsync();
-    }
-
-    private async Task ExecuteConnectAsync()
-    {
-        try
+        // ICommand properties for binding buttons to actions
+        public ICommand ConnectCommand
         {
-            // Attempt to login and check if it was successful
-            bool loginSuccessful = await AuthMicrosoftService.Login();
-            if (loginSuccessful)
+            get;
+        }
+        public ICommand DisconnectCommand
+        {
+            get;
+        }
+
+        // Visibility properties for Connect and Disconnect buttons
+        public Visibility ConnectButtonVisibility
+        {
+            get => _connectButtonVisibility;
+            set => SetProperty(ref _connectButtonVisibility, value);
+        }
+
+        public Visibility DisconnectButtonVisibility
+        {
+            get => _disconnectButtonVisibility;
+            set => SetProperty(ref _disconnectButtonVisibility, value);
+        }
+
+        // Property to hold the application's version number
+        public string VersionNumber
+        {
+            get => _versionNumber;
+            set => SetProperty(ref _versionNumber, value);
+        }
+
+        // Property to control the visibility of the welcome message
+        public Visibility WelcomeMessageVisibility => IsLoggedIn ? Visibility.Visible : Visibility.Collapsed;
+
+        // Property to control the visibility of the login prompt
+        public Visibility LoginPromptVisibility => IsLoggedIn ? Visibility.Collapsed : Visibility.Visible;
+
+        // Property to track the login status
+        public bool IsLoggedIn
+        {
+            get => _isLoggedIn;
+            private set
             {
-                // If login is successful, retrieve the user's display name and update the UI
-                DisplayName = await _microsoftGraphService.GetUserDisplayNameAsync();
-                UpdateUI(true); // isLoggedIn is set to true
+                SetProperty(ref _isLoggedIn, value);
+                OnPropertyChanged(nameof(LoginPromptVisibility));
+                OnPropertyChanged(nameof(WelcomeMessageVisibility));
             }
-            else
+        }
+
+        // Property to display the user's display name
+        public string DisplayName
+        {
+            get => _displayName;
+            set
             {
-                // If login was not successful, ensure that the UI is set to a logged out state
+                SetProperty(ref _displayName, value);
+                OnPropertyChanged(nameof(WelcomeMessageVisibility));
+            }
+        }
+
+        // Retrieves the version number of the application
+        private string GetVersionNumber()
+        {
+            var version = Assembly.GetExecutingAssembly().GetName().Version;
+            return $"{version.Major}.{version.Minor}.{version.Build}";
+        }
+
+        // Updates the UI based on the user's login status
+        private void UpdateUI(bool isLoggedIn)
+        {
+            IsLoggedIn = isLoggedIn;
+            ConnectButtonVisibility = isLoggedIn ? Visibility.Collapsed : Visibility.Visible;
+            DisconnectButtonVisibility = isLoggedIn ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        // Command execution methods for login and logout
+        private async void ExecuteConnect()
+        {
+            await ExecuteConnectAsync();
+        }
+
+        private async Task ExecuteConnectAsync()
+        {
+            try
+            {
+                bool loginSuccessful = await AuthMicrosoftService.Login();
+                if (loginSuccessful)
+                {
+                    DisplayName = await _microsoftGraphService.GetUserDisplayNameAsync();
+                    UpdateUI(true);
+                }
+                else
+                {
+                    DisplayName = string.Empty;
+                    UpdateUI(false);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error during login: {ex}");
+                UpdateUI(false);
+            }
+        }
+
+        private async void ExecuteDisconnect()
+        {
+            await ExecuteDisconnectAsync();
+        }
+
+        private async Task ExecuteDisconnectAsync()
+        {
+            try
+            {
+                await AuthMicrosoftService.Logout();
                 DisplayName = string.Empty;
-                UpdateUI(false); // isLoggedIn is set to false
+                UpdateUI(false);
             }
-        }
-        catch (Exception ex)
-        {
-            // If an exception is caught, log it and set the UI to a logged out state
-            Debug.WriteLine($"Error during login: {ex}");
-            UpdateUI(false); // isLoggedIn is set to false
-        }
-    }
-
-    private async void ExecuteDisconnect()
-    {
-        await ExecuteDisconnectAsync();
-    }
-
-    private async Task ExecuteDisconnectAsync()
-    {
-        try
-        {
-            await AuthMicrosoftService.Logout();
-            DisplayName = string.Empty;
-            UpdateUI(false);
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine($"Error during logout: {ex}");
-            UpdateUI(true);
-        }
-    }
-
-    private async void SilentLogin()
-    {
-        try
-        {
-            bool isLoggedIn = await AuthMicrosoftService.SilentLogin();
-            if (isLoggedIn)
+            catch (Exception ex)
             {
-                DisplayName = await _microsoftGraphService.GetUserDisplayNameAsync();
+                Debug.WriteLine($"Error during logout: {ex}");
+                UpdateUI(true);
             }
-            UpdateUI(isLoggedIn);
         }
-        catch (Exception ex)
+
+        // Attempts a silent login without user interaction
+        private async void SilentLogin()
         {
-            Debug.WriteLine($"Error during silent login: {ex}");
-            UpdateUI(false);
+            try
+            {
+                bool isLoggedIn = await AuthMicrosoftService.SilentLogin();
+                if (isLoggedIn)
+                {
+                    DisplayName = await _microsoftGraphService.GetUserDisplayNameAsync();
+                }
+                UpdateUI(isLoggedIn);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error during silent login: {ex}");
+                UpdateUI(false);
+            }
         }
-    }
 
-    private void UpdateUI(bool isLoggedIn)
-    {
-        ConnectButtonVisibility = isLoggedIn ? Visibility.Collapsed : Visibility.Visible;
-        DisconnectButtonVisibility = isLoggedIn ? Visibility.Visible : Visibility.Collapsed;
-    }
-
-    private async void ExecuteTest()
-    {
-        DisplayName = await _microsoftGraphService.GetUserDisplayNameAsync();
+        // Checks for application updates
+        public async Task CheckForUpdates(UIElement uiElement)
+        {
+            await _updateViewModel.CheckForUpdates(uiElement);
+        }
     }
 }
