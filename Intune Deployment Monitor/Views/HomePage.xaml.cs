8eb273288f2ff1d9fc0f82794e5beb9f912b203f
Mickaël CHAVE <mchave3@live.fr>
2023-12-02 19:10:18 +0100

    Avancement de la page d'accueil. Possibilité d'ouvrir une page internet en appyant sur un bouton.

--- Intune Deployment Monitor/Views/HomePage.xaml.cs
+++ Intune Deployment Monitor/Views/HomePage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Intune_Deployment_Monitor.ViewModels;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace Intune_Deployment_Monitor.Views;
@@ -15,13 +16,36 @@ public sealed partial class HomePage : Page
         ViewModel = App.GetService<HomeViewModel>();
         InitializeComponent();
 
-        // Assuming you want to check for updates when the HomePage is loaded
         Loaded += HomePage_Loaded;
     }
 
     private async void HomePage_Loaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
     {
-        // Pass 'this' as the UI element to the CheckForUpdates method
         await ViewModel.CheckForUpdates(this);
     }
+
+    private async void ButtonAzure_Click(object sender, RoutedEventArgs e)
+    {
+        var uri = new Uri("https://www.portal.azure.com");
+
+        // Launch the web URL
+        await Windows.System.Launcher.LaunchUriAsync(uri);
+    }
+
+    private async void ButtonIntune_Click(object sender, RoutedEventArgs e)
+    {
+        var uri = new Uri("https://intune.microsoft.com/");
+
+        // Launch the web URL
+        await Windows.System.Launcher.LaunchUriAsync(uri);
+    }
+
+    private async void ButtonGithub_Click(object sender, RoutedEventArgs e)
+    {
+        var uri = new Uri("https://github.com/mchave3/Intune-Deployment-Monitor");
+
+        // Launch the web URL
+        await Windows.System.Launcher.LaunchUriAsync(uri);
+    }
+
 }
